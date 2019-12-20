39de5cd8fcba3468a4663405df23839672b030e1
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-19 22:47:30 -0500

    Rename function for clarity

--- windows/setup/packageLists/installers.ps1
+++ windows/setup/packageLists/installers.ps1
@@ -2,7 +2,7 @@ $downloadDir = "~/Downloads"
 
 $client = new-object System.Net.WebClient
 
-function dl($url, $filename, $args){
+function ManualDLInstall($url, $filename, $args){
     $target = [IO.Path]::Combine($downloadDir, $filename)
     Write-Host $target @colorFeedback
     Write-Host "Downloading" @colorFeedback
@@ -10,15 +10,15 @@ function dl($url, $filename, $args){
     $command = Resolve-Path $target
     $command = '"' + $command + '" ' + $args
     Write-Host "Waiting for installer to finish" @colorFeedback
-    iex "& $command"
+    Invoke-Expression "& $command"
     $name = $filename -replace ".exe"
     Wait-Process -Name "$name"
 }
 
 ### Not in choco
-# dl "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP" "battle.net installer.exe"
-dl "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe" "Rockstar Games Launcher installer.exe"
-dl "https://download.info.apple.com/Mac_OS_X/041-0257.20120611.MkI85/AirPortSetup.exe" "Apple AirPortSetup.exe" "/passive /norestart"
+# ManualDLInstall "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP" "battle.net installer.exe"
+ManualDLInstall "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe" "Rockstar Games Launcher installer.exe"
+ManualDLInstall "https://download.info.apple.com/Mac_OS_X/041-0257.20120611.MkI85/AirPortSetup.exe" "Apple AirPortSetup.exe" "/passive /norestart"
 
 ### Not working in choco
 # Karen

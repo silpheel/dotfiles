565d6cad97a9c61571ae016ab90ee82392a78f87
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-22 00:49:53 -0500

    Add first time run installer portion

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,37 @@
 # Profile for the Microsoft.Powershell Shell, only. (Not Visual Studio or other PoSh instances)
 # ===========
 try {if(Get-Command "DotfileLoaded" -ErrorAction stop){}}
-Catch {Invoke-Expression ". ~/.dotfiles/windows/functions/common.ps1"}
+Catch {Invoke-Expression ". ~/.dotfiles/windows/source/function.ps1"}
 DotfileLoaded
 
+$installComplete = Get-Content env:\DOTFILES_INSTALLED
+if ($installComplete -eq "0") {
+  Write-Host "Dotfiles installation" @colorFeedback
+  Invoke-Expression ". ./software.ps1"
+  Invoke-Expression ". ./symbolic.ps1"
+  Invoke-Expression ". ./windows.ps1"
+  [Environment]::SetEnvironmentVariable("DOTFILES_INSTALLED", "1", "User")
+  # at this point alacritty should be ready to take over
+  Shell-ReloadAdminAdmin
+}
+
+# support for unicode if not envabled by default
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 $locations =
-"functions/console",
-"functions/coreaudio",
-"functions/export",
-"functions/jarvis",
-"functions/registry",
-"functions/wsl",
-"../source/alias_windows"
+"source/alias",     # shorthand and alternatives
+"source/console",   # console behavior changes
+"source/coreaudio", # audio related functions (valume, mute...)
+"source/export",    # session environment variables
+"source/jarvis",    # experiment in organizing the dotfile options
+"source/registry",  # registry manipulation functions
+"source/wsl"        # Windows Subsystem for Linux tweaks
 
 Push-Location ~\.dotfiles\windows
 $locations | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
 
+# change tab completion behavior
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+Append-EnvPath $env:USERPROFILE\.dotfiles\home\.local\bin
--- windows/setup/symbolic.ps1
+++ windows/setup/symbolic.ps1
@@ -2,6 +2,9 @@ try {if(Get-Command "DotfileLoaded" -ErrorAction stop){}}
 Catch {Invoke-Expression ". ~/.dotfiles/windows/source/function.ps1"}
 DotfileLoaded
 
+try {if(Get-Command "wsl" -ErrorAction stop){}}
+Catch {Invoke-Expression ". ~/.dotfiles/source/alias.ps1"}
+
 ### SCRIPT START
 
 Write-Host "Setting up symlinks" @colorFeedback
@@ -14,10 +17,29 @@ if ((Get-Item $profileDir).Attributes.ToString().Contains("ReparsePoint"))  # Al
 New-Item -Value $env:userprofile\.dotfiles\windows -Path $profileDir\ -ItemType Junction
 Write-Host "$dest" @colorRegular
 
-$repo = $env:userprofile\.dotfiles\home\.config
+$repo = "$env:userprofile\.dotfiles\home\.config"
 
 # PowerShell
 Symlink $env:userprofile\.dotfiles\windows\Microsoft.PowerShell_profile.ps1 $env:userprofile\.dotfiles\windows\Microsoft.PowerShellISE_profile.ps1
+
+# Windows Subsystem for Linux
+$WSLHome = (Get-ChildItem $WSLRoot\home)[0].FullName
+symlink $env:userprofile\.dotfiles\home\.bashrc $WSLHome\.bashrc
+symlink $env:userprofile\.dotfiles\home\.bash_profile $WSLHome\.bash_profile
+# TODO: dynamic link
+# this requires UNIX password
+bash /mnt/c/Users/silph/.dotfiles/windows/setup/bash_setup_folders
+symlink $env:userprofile\.dotfiles\source\alias $WSLHome\.dotfiles\source\alias
+symlink $env:userprofile\.dotfiles\source\bootstrap $WSLHome\.dotfiles\source\bootstrap
+symlink $env:userprofile\.dotfiles\source\function $WSLHome\.dotfiles\source\function
+symlink $env:userprofile\.dotfiles\source\local_config_templates $WSLHome\.dotfiles\source\local_config_templates
+symlink $env:userprofile\.dotfiles\source\path $WSLHome\.dotfiles\source\path
+symlink $env:userprofile\.dotfiles\source\prompt_string $WSLHome\.dotfiles\source\prompt_string
+symlink $env:userprofile\.dotfiles\source\ui_ux $WSLHome\.dotfiles\source\ui_ux
+symlink $env:userprofile\.dotfiles\source\xdg_base_directory_specification $WSLHome\.dotfiles\source\xdg_base_directory_specification
+# TODO: dynamic link
+# this requires UNIX password... again
+bash /mnt/c/Users/silph/.dotfiles/windows/setup/bash_setup_chmod
 
 # Alacritty
 symlink $repo\alacritty\alacritty_windows.yml $env:appdata\alacritty\alacritty.yml
@@ -30,13 +52,13 @@ symlink $repo\atom\init.coffee ~\.atom\init.coffee
 symlink $repo\atom\styles.less ~\.atom\styles.less
 
 # ShareX
-$documentsFolder = [environment]::getfolderpath("mydocuments")
+$documentsFolder = [Environment]::GetFolderPath("mydocuments")
 symlink $repo\sharex\ApplicationConfig.json $documentsFolder\ShareX\ApplicationConfig.json
 symlink $repo\sharex\HotkeysConfig.json $documentsFolder\ShareX\HotkeysConfig.json
 symlink $repo\sharex\UploadersConfig.json $documentsFolder\ShareX\UploadersConfig.json
 
 # Switcheroo - folder has version number and probably will change eventually
-$switcherooFolder = gci $env:localappdata\switcheroo | gci
+$switcherooFolder = Get-ChildItem $env:localappdata\switcheroo | Get-ChildItem
 symlink $repo\switcheroo\user.config $switcherooFolder\user.config
 
 # Wox

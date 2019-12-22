651d192885e56c0074259cc3edd82e6ea834d5d2
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-22 15:46:37 -0500

    Update routine to run, pass args and wait for installers

--- windows/setup/packageLists/installers.ps1
+++ windows/setup/packageLists/installers.ps1
@@ -1,18 +1,26 @@
 $downloadDir = "$env:userprofile/Downloads"
 
-$client = new-object System.Net.WebClient
-
-function ManualDLInstall($url, $filename, $args){
+function ManualDLInstall
+{
+    Param(
+        [Parameter(Mandatory=$true)][string] $url,
+        [Parameter(Mandatory=$true)][string] $filename,
+        [Parameter(Mandatory=$false)][string] $argString = ""
+	)
     $target = [IO.Path]::Combine($downloadDir, $filename)
     Write-Host $target @colorFeedback
-    Write-Host "Downloading" @colorFeedback
-    Invoke-WebRequest -Uri $url -OutFile $target
-    $command = Resolve-Path $target
-    $command = '"' + $command + '" ' + $args
-    Write-Host "Waiting for installer to finish" @colorFeedback
-    Invoke-Expression "& $command"
-    $name = $filename -replace ".exe"
-    Wait-Process -Name "$name"
+    Write-Host "Downloading " @colorFeedback -nonewline
+    (New-Object Net.WebClient).DownloadFile($url, $target);
+    Write-Host "ok" @colorSuccess
+    Write-Host "Installing " @colorFeedback -nonewline
+    Start-Process -Wait -ArgumentList $argString $target
+    if ($?){
+		Write-Host "ok" @colorSuccess
+        Write-Host " " @colorRegular
+	} else {
+		Write-Host "error" @colorFailure
+        Write-Host " " @colorRegular
+	}
 }
 
 # PowerShell Core Preview (current stable does not accept arguments)
@@ -20,7 +28,8 @@ $metadata = Invoke-RestMethod https://raw.githubusercontent.com/PowerShell/Power
 $release = $metadata.PreviewReleaseTag -replace '^v'
 $architecture = "x64"
 $packageName = "PowerShell-${release}-win-${architecture}.msi"
-ManualDLInstall $packageName "powershell-core-${release}.msi" "/quiet /passive /norestart"
+$downloadUrl = "https://github.com/PowerShell/PowerShell/releases/download/v${release}/${packageName}"
+ManualDLInstall $downloadUrl "powershell-core-${release}.msi" "/passive /norestart"
 
 ### Not in choco
 # ManualDLInstall "https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP" "battle.net installer.exe"
@@ -32,4 +41,4 @@ ManualDLInstall "https://download.info.apple.com/Mac_OS_X/041-0257.20120611.MkI8
 # Ultracopier
 
 ### Latest in Choco is unstable
-dl "https://download.kde.org/stable/digikam/6.4.0/digiKam-6.4.0-Win64.exe" "digiKam 6.4.0 installer.exe"
+ManualDLInstallExe "https://download.kde.org/stable/digikam/6.4.0/digiKam-6.4.0-Win64.exe" "digiKam 6.4.0 installer.exe"

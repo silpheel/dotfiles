7f19ca3f0185b402b3a52bd27f9ea8bf015e5f4e
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-27 12:03:27 -0500

    Add new aliases, lint

--- windows/source/alias.ps1
+++ windows/source/alias.ps1
@@ -1,6 +1,5 @@
-$Time = [System.Diagnostics.Stopwatch]::StartNew()
-try {if(Get-Command "DotfileLoaded" -ErrorAction stop){}}
-Catch {Invoke-Expression ". $env:userprofile/.dotfiles/windows/source/function.ps1"}
+try { if (Get-Command "DotfileLoaded" -ErrorAction stop) {} }
+catch { Invoke-Expression ". $env:USERPROFILE/.dotfiles/windows/source/function.ps1" }
 DotfileLoaded
 
 # Easier Navigation: .., ..., ...., ....., and ~
@@ -13,28 +12,48 @@ ${Function:.....} =  { Set-Location ..\..\..\.. }
 ${Function:......} = { Set-Location ..\..\..\..\.. }
 
 # Navigation Shortcuts
-${Function:drop} =     { Set-Location ~\Documents\Dropbox }
-${Function:dt} =       { Set-Location ~\Desktop }
-${Function:docs} =     { Set-Location ~\Documents }
-${Function:dl} =       { Set-Location ~\Downloads }
-${Function:local} =    { Set-Location ~\AppData\Local }
-${Function:locallow} = { Set-Location ~\AppData\LocalLow }
-${Function:roaming} =  { Set-Location ~\AppData\Roaming }
-${Function:posh} =     { Set-Location (Split-Path -parent $profile)}
+${Function:.drop} = { Set-Location ~\Documents\Dropbox }
+${Function:.dt} = { Set-Location ~\Desktop }
+${Function:.win} = { Set-Location ~\.dotfiles\windows }
+${Function:.dotfiles} = { Set-Location ~\.dotfiles }
+${Function:.docs} = { Set-Location ~\Documents }
+${Function:.dl} = { Set-Location ~\Downloads }
+${Function:.local} = { Set-Location ~\AppData\Local }
+${Function:.locallow} = { Set-Location ~\AppData\LocalLow }
+${Function:.roaming} = { Set-Location ~\AppData\Roaming }
+${Function:.posh} = { Set-Location (Split-Path -Parent $profile) }
+${Function:.temp} = { Set-Location $env:TEMP }
+
+# Used by Jarvis
+$Global:JarvisShortcuts = @{
+  "Dropbox" = ".drop";
+  "Desktop" = ".dt";
+  "Windows_dotfiles" = ".win";
+  "Dotfiles_root" = ".root";
+  "Documents" = ".docs";
+  "Downloads" = ".dl";
+  "LocalData" = ".local";
+  "LocalLowData" = ".locallow";
+  "RoamingData" = ".roaming";
+  "PowerShellProfile" = ".posh";
+  "User_profile" = "~";
+  "Temp" = ".temp";
+}
+
 # quick close
 ${Function:Get-Out} = { exit }; Set-Alias "x" Get-Out
 
-try {if(Get-Command "Test-RegistryValue" -ErrorAction stop){}}
-Catch {Invoke-Expression ". $env:userprofile/.dotfiles/windows/source/registry.ps1"}
+try { if (Get-Command "Test-RegistryValue" -ErrorAction stop) {} }
+catch { Invoke-Expression ". $env:USERPROFILE/.dotfiles/windows/source/registry.ps1" }
 
 # Windows Subsystem for Linux
 $RegWindowsCV = 'HKCU:\Software\Microsoft\Windows\CurrentVersion'
-if (Test-Path "$RegWindowsCV\Lxss" -And Test-RegistryValue "$RegWindowsCV\Lxss" "DefaultDistribution") {
-    $WSLDistro = (Get-ItemProperty -Path "$RegWindowsCV\Lxss" -Name 'DefaultDistribution').DefaultDistribution
-    $WSLRootFS = (Get-ItemProperty -Path "$RegWindowsCV\Lxss\$WSLDistro" -Name 'BasePath').BasePath
-    ${Function:wsl} = {
-      Set-Location $WSLRootFS
-    }
+if ((Test-Path "$RegWindowsCV\Lxss") -and (Test-RegistryValue "$RegWindowsCV\Lxss" "DefaultDistribution")) {
+  $WSLDistro = (Get-ItemProperty -Path "$RegWindowsCV\Lxss" -Name 'DefaultDistribution').DefaultDistribution
+  $WSLRootFS = (Get-ItemProperty -Path "$RegWindowsCV\Lxss\$WSLDistro" -Name 'BasePath').BasePath
+  ${Function:wsl} = {
+    Set-Location $WSLRootFS
+  }
 }
 
 # boostrap and reload
@@ -50,24 +69,21 @@ Set-Alias time Measure-Command
 # Correct PowerShell Aliases if tools are available (aliases win if set)
 # WGet: Use `wget.exe` if available
 if (Get-Command wget.exe -ErrorAction SilentlyContinue | Test-Path) {
-  rm alias:wget -ErrorAction SilentlyContinue
+  Remove-Item alias:wget -ErrorAction SilentlyContinue
 }
 
-$LS_DEFAULTS="-lAhviNp"
-$LS_IGNORES="--ignore=.DS_Store --ignore='Icon'$'\r' --ignore=.CFUserTextEncoding --ignore=.localized --ignore=.Trash --ignore=.Trashes --ignore=.Spotlight-V100 --ignore=.fseventsd"
+$LS_DEFAULTS = "-lAhviNp"
+$LS_IGNORES = "--ignore=.DS_Store --ignore='Icon'$'\r' --ignore=.CFUserTextEncoding --ignore=.localized --ignore=.Trash --ignore=.Trashes --ignore=.Spotlight-V100 --ignore=.fseventsd"
 ${Function:ll} = { ls $LS_DEFAULTS $LS_IGNORES }
-${Function:lll} = { ls $LS_DEFAULTS $LS_IGNORES | Out-Host -paging}
+${Function:lll} = { ls $LS_DEFAULTS $LS_IGNORES | Out-Host -Paging }
 
 # curl: Use `curl.exe` if available
 if (Get-Command curl.exe -ErrorAction SilentlyContinue | Test-Path) {
-    rm alias:curl -ErrorAction SilentlyContinue
-    # Set `ls` to call `ls.exe` and always use --color
-    ${Function:curl} = { curl.exe @args }
-    # Gzip-enabled `curl`
-    ${Function:gurl} = { curl --compressed @args }
+  Remove-Item alias:curl -ErrorAction SilentlyContinue
+  ${Function:curl} = { curl.exe @args }
+  ${Function:gurl} = { curl --compressed @args }
 } else {
-    # Gzip-enabled `curl`
-    ${Function:gurl} = { curl -TransferEncoding GZip }
+  ${Function:gurl} = { curl -TransferEncoding GZip }
 }
 
 # Create a new directory and enter it
@@ -88,3 +104,7 @@ Set-Alias reload Reload-Powershell
 # http://xkcd.com/530/
 Set-Alias mute Set-SoundMute
 Set-Alias unmute Set-SoundUnmute
+
+Set-Alias mkd CreateAndSet-Directory
+
+Set-Alias vi vim

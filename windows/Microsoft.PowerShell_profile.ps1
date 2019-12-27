05a0431bbe342f350f550b1227e7781b955f6939
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-27 11:34:37 -0500

    Lint, autorestart shell after install

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,21 @@
 # Profile for the Microsoft.Powershell Shell, only. (Not Visual Studio or other PoSh instances)
 # ===========
-try {if(Get-Command "DotfileLoaded" -ErrorAction stop){}}
-Catch {Invoke-Expression ". $env:userprofile/.dotfiles/windows/source/function.ps1"}
+try { if (Get-Command "DotfileLoaded" -ErrorAction stop) {} }
+catch { Invoke-Expression ". $env:USERPROFILE/.dotfiles/windows/source/function.ps1" }
 DotfileLoaded
 
 # support for unicode if not enabled by default
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-$installComplete = [Environment]::GetEnvironmentVariable("DOTFILES_INSTALLED", "User")
+$installComplete = [Environment]::GetEnvironmentVariable("DOTFILES_INSTALLED","User")
 if ($installComplete -eq "0") {
   Write-Host "Dotfiles installation" @colorFeedback
-  Invoke-Expression ". $env:userprofile/.dotfiles/windows/setup/software.ps1"
-  Invoke-Expression ". $env:userprofile/.dotfiles/windows/setup/windows.ps1"
-  Invoke-Expression ". $env:userprofile/.dotfiles/windows/setup/symbolic.ps1"
-  [Environment]::SetEnvironmentVariable("DOTFILES_INSTALLED", "1", "User")
+  Invoke-Expression "$env:USERPROFILE/.dotfiles/windows/setup/software.ps1"
+  Invoke-Expression "$env:USERPROFILE/.dotfiles/windows/setup/windows.ps1"
+  Invoke-Expression "$env:USERPROFILE/.dotfiles/windows/setup/symbolic.ps1"
+  [Environment]::SetEnvironmentVariable("DOTFILES_INSTALLED","1","User")
   # at this point alacritty should be ready to take over
-  # Shell-ReloadAdminAdmin
-  echo "we would exit here"
-  break
+  Restart-AdminShell
 }
 
 $locations =
@@ -29,11 +27,13 @@ $locations =
 "source/registry",  # registry manipulation functions
 "source/wsl"        # Windows Subsystem for Linux tweaks
 
-Push-Location $env:userprofile\.dotfiles\windows
-$locations | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
+Push-Location $env:USERPROFILE\.dotfiles\windows
+$locations | Where-Object { Test-Path "$_.ps1" } | ForEach-Object -Process {
+  Invoke-Expression ". .\$_.ps1"
+}
 Pop-Location
 
 # change tab completion behavior
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
 Append-EnvPath $env:USERPROFILE\.dotfiles\home\.local\bin

1851d75e3e2be43abaedefca2834639ae647e157
Edoardo Facchinelli <silpheel@gmail.com>
2019-12-27 11:43:57 -0500

    Lint

--- windows/setup/upgrade.ps1
+++ windows/setup/upgrade.ps1
@@ -1,15 +1,15 @@
-try {if(Get-Command "DotfileLoaded" -ErrorAction stop){}}
-Catch {Invoke-Expression ". $env:userprofile/.dotfiles/windows/source/function.ps1"}
+try { if (Get-Command "DotfileLoaded" -ErrorAction stop) {} }
+catch { Invoke-Expression "$env:USERPROFILE/.dotfiles/windows/source/function.ps1" }
 DotfileLoaded
 
-try {if(Get-Command "Verify-Elevated" -ErrorAction stop){}}
-Catch {Invoke-Expression ". $env:userprofile/.dotfiles/windows/source/console.ps1"}
+try { if (Get-Command "Verify-Elevated" -ErrorAction stop) {} }
+catch { Invoke-Expression "$env:USERPROFILE/.dotfiles/windows/source/console.ps1" }
 
 # Check to see if we are currently running "as Administrator"
 if (!(Verify-Elevated)) {
-    Write-Host "You need to start the shell as Administrator mode for this." @colorError -nonewline
-    Write-Host "" @colorRegular
-    break
+  Write-Host "You need to start the shell as Administrator mode for this." @colorError -NoNewline
+  Write-Host "" @colorRegular
+  break
 }
 
 ### Update Help for Modules
@@ -18,11 +18,11 @@ Update-Help -Force
 
 ### Chocolatey
 if (which choco) {
-    choco upgrade all
+  choco upgrade all
 }
 
 ### Atom Packages
 if (which apm) {
-    Invoke-Expression ". $env:userprofile\.dotfiles\windows\setup\packageLists\atom.ps1"
-    Command-ManagerLoop -Command "upgrade" -packageList $atomPackageList -manager "apm"
+  Invoke-Expression "$env:USERPROFILE\.dotfiles\windows\setup\packageLists\atom.ps1"
+  Command-ManagerLoop -Command "upgrade" -packageList $atomPackageList -manager "apm"
 }
